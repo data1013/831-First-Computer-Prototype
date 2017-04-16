18f33603d2d5399c28f49d60c5817e114fdcf056
Karen <cwfan@mit.edu>
2017-04-16 16:25:37 -0400

    did the filter stuff

--- js/explore.js
+++ js/explore.js
@@ -1,6 +1,11 @@
 $( document ).ready(function() {
   $('.ui.dropdown').dropdown();  
-  $("#stock-image").on("mousedown", function() {
+  $("#stock-image").on("click", function() {
     $('.ui.modal').modal('show');  
   });
+    
+  $(".item").on("click", function() {
+    console.log($(this).attr("data-value"));
+  })
 });
+
